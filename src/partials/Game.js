72a2b683359cc45d94d702f94dbbd70412cf8dad
fyeung21 <yeung.fiona@hotmail.com>
2019-05-07 21:05:02 -0700

    added more to svg in js

--- src/partials/Game.js
+++ src/partials/Game.js
@@ -1,15 +1,25 @@
+
+import {SVG_NS} from '../settings';
+import Board from './Board';
+
 export default class Game {
   constructor(element, width, height) {
     this.element = element;
     this.width = width;
     this.height = height;
-    let svg = document.createElementNS(SVG_NS, 'svg');
+    this. svg = document.createElementNS(SVG_NS, 'svg');
     this.gameElement = document.getElementById(this.element);
-    this.gameElement.appendChild(svg);
-		// Other code goes here...
+    svg.setAttributeNS(null, "width", this.width);
+    svg.setAttributeNS(null, "height", this.height);
+    svg.setAttributeNS(null, "viewBox",`0 0 ${this.width} ${this.height}`);
+    this.gameElement.appendChild(this.svg);
+    this.board = new Board(this.width,this.height);
+	
   }
 
   render() {
+    
 		// More code goes here....
-  }
+  this.board.render(this.svg);
+}
 }
